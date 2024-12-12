2f2a0a5e56adc3989474f4fa7fe5fa194451fd3a
nikita.savin <nikita.savin@simbirsoft.com>
2024-12-12 21:35:43 +0300

    refactor: настроить генерацию автодокументации

--- src/presentation/app.py
+++ src/presentation/app.py
@@ -1,12 +1,18 @@
 from fastapi import APIRouter, FastAPI
 
+from src.core.settings import Settings
 from src.presentation.routers import router
 
 
 def _create_app(router: APIRouter):
+    settings = Settings()
     app = FastAPI(
         title="AR-Converter API",
         description="This service provides online API-converter from different kinds of AR-files to Apple-compartible USDZ-file",
+        docs_url="/docs" if settings.app.mount_swagger else None,
+        redoc_url="/redoc" if settings.app.mount_redoc else None,
+        openapi_url="/openapi.json" if settings.app.mount_swagger or settings.app.mount_redoc else None
+
     )
 
     app.include_router(router)
